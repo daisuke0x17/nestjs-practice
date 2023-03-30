d0b9a60587e1e17e79e5a42132bf664c8756f9f6
katayaman <daisuke23bubu@gmail.com>
2023-03-30 22:40:12 +0900

    feat: auth module

--- nest-introduction/src/migrations/1680183343841-CreateUser.ts
+++ nest-introduction/src/migrations/1680183343841-CreateUser.ts
@@ -1,13 +1,15 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class CreateItem1679830260207 implements MigrationInterface {
-    name = 'CreateItem1679830260207'
+export class CreateUser1680183343841 implements MigrationInterface {
+    name = 'CreateUser1680183343841'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "item" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "price" integer NOT NULL, "description" character varying NOT NULL, "status" character varying NOT NULL, "createdAt" character varying NOT NULL, "updatedAt" character varying NOT NULL, CONSTRAINT "PK_d3c0c71f23e7adcf952a1d13423" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "userName" character varying NOT NULL, "password" character varying NOT NULL, "status" character varying NOT NULL, CONSTRAINT "UQ_da5934070b5f2726ebfd3122c80" UNIQUE ("userName"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "user"`);
         await queryRunner.query(`DROP TABLE "item"`);
     }
 
