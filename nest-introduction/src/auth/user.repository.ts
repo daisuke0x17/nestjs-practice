bf5160dcf8285b3e58d315e5b32c67a76c1a1bae
katayaman <daisuke23bubu@gmail.com>
2023-04-04 20:00:56 +0900

    feat: description of jwt

--- nest-introduction/src/auth/user.repository.ts
+++ nest-introduction/src/auth/user.repository.ts
@@ -1,12 +1,16 @@
 import { User } from 'src/entities/user.entity';
 import { EntityRepository, Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(CreateUserDto: CreateUserDto): Promise<User> {
     const { userName, password, status } = CreateUserDto;
-    const user = this.create({ userName, password, status });
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ userName, password: hashPassword, status });
 
     await this.save(user);
     return user;
